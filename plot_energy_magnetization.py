16906a8e0de1a5a1e6d772460b468864c93ff277
Pitahaya96 <Chrisschroeder96@gmx.de>
2022-01-19 09:14:27 +0100

    update plots

--- plot_energy_magnetization.py
+++ plot_energy_magnetization.py
@@ -1,26 +1,63 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
+                               AutoMinorLocator)
+from numpy.core.function_base import linspace
 
+L=32
 
-x=np.loadtxt("energy+magnetization_B=0.000000_J=1.000000_beta=5.000000.txt")
+plt.rcParams["figure.figsize"] = (7, 4.5)
+
+x=np.loadtxt("energy+magnetization_B=0.000000_J=1.000000_beta=0.500000.txt")
 y = x.astype(float)
 
 energy = np.zeros(len(y))
 magnetization = np.zeros(len(y))
+aU = 0
+aM = 0
+
 for i in range(len(y)):
-    energy[i] = y[i][0]
-    magnetization[i] = y[i][1]
-    
-f1 = plt.figure()
-plt.plot(energy)
-plt.xlabel("Steps")
-plt.ylabel("Energy")
-plt.savefig("plot_energy_beta=0.9.pdf")
+    energy[i] = y[i][0]/(L*L)
+    aU = aU + y[i][0]/(L*L)
+    magnetization[i] = y[i][1]/(L*L)
+    aM = aM + y[i][1]/(L*L)
 
-f2 = plt.figure()
-plt.plot(magnetization)
-plt.xlabel("Steps")
-plt.ylabel("Magnetisation")
-plt.savefig("plot_magnetisation_beta=0.9.pdf")
+aU = aU / len(y)
+aM = aM / len(y)
 
+x1=linspace(0,2000)
+y1=linspace(aU,aU)
+y2=linspace(aM,aM)
+
+fig, axs = plt.subplots(2,1)
+
+axs[0].plot(energy,'.')
+axs[0].plot(x1,y1,'r')
+axs[0].set_ylabel("U/n",fontsize=18)
+axs[0].set_ylim(-0.37,-0.19)
+axs[0].tick_params(axis='both', which='major', labelsize=12)
+axs[0].yaxis.set_major_locator(MultipleLocator(0.05))
+axs[0].xaxis.set_major_locator(MultipleLocator(500))
+axs[0].yaxis.set_minor_locator(MultipleLocator(0.01))
+axs[0].xaxis.set_minor_locator(MultipleLocator(50))
+axs[0].grid(True)
+
+axs[1].plot(magnetization,'.')
+axs[1].plot(x1,y2,'r')
+axs[1].set_xlabel("N",fontsize=18)
+axs[1].set_ylabel("M/n",fontsize=18)
+axs[1].set_ylim(-0.12,0.12)
+axs[1].tick_params(axis='both', which='major', labelsize=12)
+axs[1].yaxis.set_major_locator(MultipleLocator(0.05))
+axs[1].xaxis.set_major_locator(MultipleLocator(500))
+axs[1].yaxis.set_minor_locator(MultipleLocator(0.01))
+axs[1].xaxis.set_minor_locator(MultipleLocator(50))
+axs[1].grid(True)
+
+plt.savefig("plot_magnetisation_energy.pdf",bbox_inches='tight')
+
+print(aU)
+input('5')
+print(aM)
+input('6')
--- plot_states.py
+++ plot_states.py
@@ -4,9 +4,7 @@ import os
 
 L = 32
 
-#f=open("state.txt","r")
-
-f=open("states_B=0.000000_J=1.000000_beta=0.500000.txt","r")
+f=open("states_B=0.000000_J=1.000000_beta=1.200000.txt","r")
 
 lines=f.readlines()
 
@@ -31,14 +29,17 @@ for index,line in enumerate(lines):
         width = i % L
         height = int(i/L)
         
-        ax.tick_params(labeltop=True, labelright=True)
+        #ax.tick_params(labelleft=False, labeldown=False)
         
         if(state[i] == '0.5'):
-            plt.plot(width,height,'.',color="tab:blue")
+            plt.plot(width,height,'s',color="tab:blue")
         else:
-            plt.plot(width,height,'.',color="tab:orange")
-            
+            plt.plot(width,height,'s',color="tab:orange")
+    
+    #plt.xlabel("Position",fontsize=16)
+    #plt.ylabel("Position",fontsize=16)
+    plt.axis('off')
     if(len(lines)==1):
-        plt.savefig("state.png",bbox_inches = 'tight')
+        plt.savefig("state.pdf",bbox_inches = 'tight')
     else:
-        plt.savefig("step_plots/step_"+"%02d" % (index,)+"_state.png",bbox_inches = 'tight')
+        plt.savefig("step_plots/step_"+"%02d" % (index,)+"_state.pdf",bbox_inches = 'tight')
